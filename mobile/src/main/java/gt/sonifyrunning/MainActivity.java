05cf9756b48d66fe93d21539de0c999d310ade35
Mihir Parshionikar <mihir.parshy@hotmail.com>
2018-04-03 17:37:15 -0400

    Working on nexus 5

--- mobile/src/main/java/gt/sonifyrunning/MainActivity.java
+++ mobile/src/main/java/gt/sonifyrunning/MainActivity.java
@@ -1,13 +1,67 @@
+/**
+ * Mihir Parshionikar. 4/3/2018
+ * Code based on https://code.tutsplus.com/tutorials/using-the-accelerometer-on-android--mobile-22125
+ **/
 package gt.sonifyrunning;
 
+import android.content.Context;
+import android.hardware.SensorEvent;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("tag1", "On Create.");
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    /*
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = sensorEvent.sensor;
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+        }
+    } */
+
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+        Log.d("sensor", "Sensor changed");
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+            Log.d("myTag", Float.toString(x));
+
+        }
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
