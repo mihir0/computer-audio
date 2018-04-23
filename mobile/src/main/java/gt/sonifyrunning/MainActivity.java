25dc988b56834f98be357b63845b496c4e33bc7f
Mihir Parshionikar <mihir.parshy@hotmail.com>
2018-04-23 13:21:05 -0400

    reads in .csv files in real time

--- mobile/src/main/java/gt/sonifyrunning/MainActivity.java
+++ mobile/src/main/java/gt/sonifyrunning/MainActivity.java
@@ -13,6 +13,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.SoundPool;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,21 +25,28 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.opencsv.CSVReader;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity /*implements SensorEventListener*/ {
-    /*
+
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
     private TextView textView;
     private EditText editText;
-    private Button startButton;
     private Date startTime;
 
-    private String baseDir, fileName, filePath; */
+    private String baseDir, fileName, filePath;
 
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
@@ -49,10 +58,15 @@ public class MainActivity extends AppCompatActivity /*implements SensorEventList
     private SoundPool soundPool;
     private int soundID = -1;
     private int streamID;
-    private int volume = 50;
+    private float volume = 50;
     private boolean soundsLoaded = false;
     private Button startButton;
+    private Button soundButton;
     private SeekBar seekBar;
+    private Handler mHandler;
+    private Timer timer;
+    private ArrayList<String[]> sensorReadings;
+    private int sensorIndex = 0; //indicates where in sensorReadings we currently are
     private void initSound() {
         Context mContext = getApplicationContext();
         soundPool = new SoundPool.Builder()
@@ -66,14 +80,38 @@ public class MainActivity extends AppCompatActivity /*implements SensorEventList
         });
     }
 
+    private ArrayList<String[]> readCSV() throws IOException{
+        ArrayList<String[]> data = new ArrayList<String[]>();
+        baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+        fileName = editText.getText() + ".csv";
+        filePath = baseDir + File.separator + fileName;
+        Log.d("File path", filePath);
+        try {
+            CSVReader reader = new CSVReader(new FileReader(filePath));
+            String[] nextLine;
+
+            while ((nextLine = reader.readNext()) != null) {
+                // nextLine[] is an array of values from the line
+                data.add(nextLine);
+            }
+        } catch(IOException e) {
+            throw e;
+        }
+        return data;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity.verifyStoragePermissions(this);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
         startButton = findViewById(R.id.startButton);
-        startButton.setOnClickListener(new View.OnClickListener() {
+        soundButton = findViewById(R.id.soundButton);
+        soundButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                streamID = soundPool.play(soundID, 1, 1, 1, 3, 1f);
+                streamID = soundPool.play(soundID, 1, 1, 1, -1, 1f);
             }
         });
         seekBar = findViewById(R.id.seekBar);
@@ -81,8 +119,8 @@ public class MainActivity extends AppCompatActivity /*implements SensorEventList
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 // updated continuously as the user slides the thumb
-                volume = progress;
-                soundPool.setVolume(streamID, (float) volume / 100, (float) volume / 100);
+                volume = (float) progress;
+                soundPool.setVolume(streamID, volume / 100, volume / 100);
             }
 
             @Override
@@ -96,6 +134,43 @@ public class MainActivity extends AppCompatActivity /*implements SensorEventList
             }
         });
 
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    active = true;
+                    timer = new Timer();
+                    sensorReadings = readCSV();
+                    startTime = Calendar.getInstance().getTime();
+                    sensorIndex = 0;
+
+                    timer.scheduleAtFixedRate(new TimerTask() {
+                        @Override
+                        public void run() {
+                            if (active) {
+                                long currentTime = Calendar.getInstance().getTime().getTime() - startTime.getTime();
+                                Log.d("Current Time:", String.valueOf(currentTime) + ", " + String.valueOf(sensorIndex));
+                                while (Long.parseLong(sensorReadings.get(sensorIndex)[1].trim()) < currentTime) {
+                                    sensorIndex++;
+                                    if (sensorIndex == sensorReadings.size()) { //End of file
+                                        timer = null;
+                                        active = false;
+                                        startTime = null;
+                                        break;
+                                    }
+                                }
+                                float x = Float.parseFloat(sensorReadings.get(sensorIndex)[2].trim());
+                                float y = Float.parseFloat(sensorReadings.get(sensorIndex)[3].trim());
+                                float z = Float.parseFloat(sensorReadings.get(sensorIndex)[4].trim());
+                                sensorUpdate(x, y, z);
+                            }
+                        }
+                    }, 0, 1000/20);
+                } catch (IOException e) {
+                    textView.setText("File not found!");
+                }
+            }
+        });
 
         /*
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -129,7 +204,7 @@ public class MainActivity extends AppCompatActivity /*implements SensorEventList
         initSound();
     }
 
-    /*
+
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (active) {
             Sensor mySensor = sensorEvent.sensor;
@@ -144,12 +219,20 @@ public class MainActivity extends AppCompatActivity /*implements SensorEventList
                 double time = Calendar.getInstance().getTime().getTime() - startTime.getTime();
                 line = String.format("%.00f, %.04f, %.04f, %.04f\n", time, x, y, z);
                 textView.setText(line);
-                writeToFile(line);
+                //writeToFile(line);
             }
         }
     }
 
-
+    public void sensorUpdate(float x, float y, float z) { //called when SIMULATING real time sensor data from stored .csv
+        float accel_mag = Math.abs(x) + Math.abs(y) + Math.abs(z);
+        String line = ""; //Time(ms), x, y ,z
+        double time = Calendar.getInstance().getTime().getTime() - startTime.getTime();
+        line = String.format("%.00f, %.04f, %.04f, %.04f\n", time, x, y, z);
+        Log.d("Sensor: ", line);
+        //textView.setText(line);
+    }
+    /*
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
